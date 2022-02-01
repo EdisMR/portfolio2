00f2fce7100c3c91296e4679afb1e4b8d555e3a5
Edis MR <edisanthony@gmail.com>
2022-02-01 09:58:54 -0600

    cover page finished

--- src/app/coverpage/coverpage.component.ts
+++ src/app/coverpage/coverpage.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoverpageComponent implements OnInit {
 
+  itemcards:{
+    imageurl:string
+    title:string
+  }[]=[
+    {imageurl:"assets/icons/psico.svg",title:"Continuous Learning Continuous Learning Continuous Learning Continuous Learning"},
+    {imageurl:"assets/icons/check.svg",title:"Eficiencia y eficacia notable Eficiencia y eficacia notable Eficiencia y eficacia notable"},
+    {imageurl:"assets/icons/list.svg",title:"Metodologias efectivas aplicadas Metodologias efectivas aplicadas Metodologias efectivas aplicadas"}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
