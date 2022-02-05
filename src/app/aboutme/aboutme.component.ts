34a425f39d6ac215ecebf8b3b6940b4adc644b67
Edis MR <edisanthony@gmail.com>
2022-02-05 10:23:36 -0600

    Style corrections and about me component added

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutmeComponent implements OnInit {
 
+  items:{
+    name:string
+    type:string
+  }[]=[
+    {name:"HTML",type:"green-border"},
+    {name:"CSS",type:"green-border"},
+    {name:"Javascript",type:"green-border"},
+    {name:"Typescript",type:"green-border"},
+    {name:"SASS",type:"green-border"},
+    {name:"Responsive Web Design",type:"green-border"},
+    {name:"Bootstrap",type:"green-border"},
+    {name:"Git",type:"green-border"},
+    {name:"Github",type:"green-border"},
+    {name:"GSAP",type:"green-border"},
+    {name:"Angular",type:"yellow-border"},
+    {name:"React",type:"yellow-border"},
+    {name:"JS REGEXP",type:"yellow-border"},
+    {name:"PHP",type:"red-border"},
+  ]
+
+  antecedentes:string[]=[
+    "uno",
+    "dos",
+    "tres",
+    "cuatro",
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
