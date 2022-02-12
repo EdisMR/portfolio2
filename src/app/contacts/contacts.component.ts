4bf4a829b4d3797b3dc7cf681a897b0ea4e5747a
Edis MR <edisanthony@gmail.com>
2022-02-12 09:31:46 -0600

    Contacts zone added

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -7,6 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  contacts:{
+    title:string
+    url:string
+    image:string/* imagen de fondo */
+  }[]=[
+    {
+      title:"LinkedIn",
+      url:"https://www.linkedin.com/in/edismr/",
+      image:""
+    },
+    {
+      title:"Github",
+      url:"https://github.com/EdisMR",
+      image:""
+    },
+    {
+      title:"Email",
+      url:"mailto:edisanthony@gmail.com",
+      image:""
+    },
+    {
+      title:"LinkedIn",
+      url:"https://www.linkedin.com/in/edismr/",
+      image:""
+    },
+    {
+      title:"Telegram",
+      url:"https://t.me/edismr",
+      image:""
+    },
+    {
+      title:"Whatsapp",
+      url:"https://wa.me/50663062581",
+      image:""
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
