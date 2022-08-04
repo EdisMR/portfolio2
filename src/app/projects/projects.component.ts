55fe84317f6df89d15a2fc8d25569b3d7554ebc6
Edis MR <edisanthony@gmail.com>
2022-08-04 06:36:09 -0600

    multimedia some videos added

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  videosNames:string[] = [
+    'assets/img/video(1).mp4',
+    'assets/img/video(2).mp4',
+    'assets/img/video(3).mp4',
+    'assets/img/video(4).mp4',
+    'assets/img/video(5).mp4',
+    'assets/img/video(6).mp4',
+    'assets/img/video(7).mp4',
+    'assets/img/video(8).mp4',
+    'assets/img/video(9).mp4',
+    'assets/img/video(10).mp4',
+    'assets/img/video(11).mp4',
+    'assets/img/video(12).mp4',
+    'assets/img/video(13).mp4',
+    'assets/img/video(14).mp4',
+  ]
+
   isMobile(){
     if(window.screen.width>=600){
       return 2
