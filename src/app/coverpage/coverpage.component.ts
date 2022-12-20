82176b1afed9e2bf225a7da5edfc7af1729e5b7f
Edis MR <edisanthony@gmail.com>
2022-12-20 09:33:02 -0600

    voer page imaeg media queries

--- src/app/coverpage/coverpage.component.ts
+++ src/app/coverpage/coverpage.component.ts
@@ -7,18 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoverpageComponent implements OnInit {
 
-  itemcards:{
-    imageurl:string
-    title:string
-  }[]=[
-    {imageurl:"assets/icons/psico.svg",
-    title:"Continuous Learning through digital platforms like Youtube, Udemy, LinkedIn Learning, among others"},
-    {imageurl:"assets/icons/check.svg",
-    title:"Remarkable efficiency and effectiveness on projects of all sizes"},
-    {imageurl:"assets/icons/list.svg",
-    title:"Effective methodologies applied, for example Kanban, Scrum, Pomodoro"}
-  ]
-
   constructor() { }
 
   ngOnInit(): void {
